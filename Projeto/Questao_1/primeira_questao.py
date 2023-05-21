d6faa225e2a0ce6bdb730439718511017b0f07e3
Josegabri <josegab12@outlook.com>
2023-05-21 19:00:07 -0300

    funções documentadas

--- Projeto/Questao_1/primeira_questao.py
+++ Projeto/Questao_1/primeira_questao.py
@@ -1,6 +1,23 @@
 # Função que define o automato 
 
-def automato(funcao_transicao,q0,conj_est_finais, cadeia):
+def automato_1(funcao_transicao,q0,conj_est_finais, cadeia):
+    '''
+      Gerador de automato 
+      Args: 
+        Transições presentes no automato -> um dicionario com strings. Tendo do lado direito o estado e o simbolo a ser analisado.
+        lado esquerdo, para qual estado irá. 
+
+        q0 -> estado inicial do automato
+
+        Estados finais do automato -> string
+
+        cadeia -> sequencia de string a ser analizada pelo automato criado
+
+      Return:
+        Print com a ilustração da ocorrência das transições 
+        
+        Valor booleano para a validação da cadeia para com o automato 
+    '''
     estado_atual=q0
     for simbolo in cadeia:
         print(f'({estado_atual}, {simbolo}) ---> {funcao_transicao.get((estado_atual, simbolo))}')
@@ -11,28 +28,48 @@ def automato(funcao_transicao,q0,conj_est_finais, cadeia):
   
 # LETRA A 
 def automato_a(cadeia:str):
+  '''
+   Validação de cadeia no automato da primeira questão, letra A
+   É usado uma chamada de outra função (automato_1), para a criação do automato,
+   sendo passado o automato desenvolvido para a alternativa
+   
+   Args: 
+    cadeia de string a ser analizada no automato criado
+
+   Return: 
+    Return da função automato. Print da transições utilizadas.
+    Valor booleano da validação da cadeia.
+  '''
   estados_a = ['q0', 'q1', 'q2']
   alfabeto_a = ['a', 'b', 'c']
   funcao_transicao_a = {
     ('q0', 'a'): 'q1',
-    ('q0', 'b'): None,
     ('q0', 'c'): 'q0',
-    ('q1', 'a'): None,
     ('q1', 'b'): 'q2',
-    ('q1', 'c'): None,
     ('q2', 'a'): 'q1',
-    ('q2', 'b'): None,
     ('q2', 'c'): 'q2'
   }
   estado_inicial_a = 'q0'
   estados_finais_a = ['q0', 'q2']
 
-  return automato(funcao_transicao_a, estado_inicial_a, estados_finais_a, cadeia)
+  return automato_1(funcao_transicao_a, estado_inicial_a, estados_finais_a, cadeia)
 
 
 
 # LETRA B
 def automato_b(cadeia:str):
+  '''
+   Validação de cadeia no automato da primeira questão, letra B
+   É usado uma chamada de outra função (automato), para a criação do automato,
+   sendo passado o automato desenvolvido para a alternativa
+   
+   Args: 
+    cadeia de string a ser analizada no automato criado
+
+   Return: 
+    Return da função automato. Print da transições utilizadas.
+    Valor booleano da validação da cadeia.
+  '''
   estados_b = ['q0','q1','q2','q3','q4','q5','q6','q7']
   alfabeto_b = ['a','b','c']
   funcao_transicao_b={
@@ -40,28 +77,19 @@ def automato_b(cadeia:str):
     ('q0','b'):'q4',
     ('q0','c'):'q4',
     ('q1','a'):'q2',
-    ('q1','b'):None,
-    ('q1','c'):None,
     ('q2','a'):'q3',
-    ('q2','b'):None,
-    ('q2','c'):None,
-    ('q3','a'):None,
     ('q3','b'):'q3',
     ('q3','c'):'q3',
     ('q4','b'):'q4',
     ('q4','c'):'q4',
     ('q4','a'):'q5',
     ('q5','a'):'q6',
-    ('q5','b'):None,
-    ('q5','c'):None,
-    ('q6','a'):'q7',
-    ('q6','b'):None,
-    ('q6','c'):None   
+    ('q6','a'):'q7',  
   }
   estado_inicial_b= 'q0'
   estados_finais_b= ['q3','q7']
 
-  return automato(funcao_transicao_b, estado_inicial_b, estados_finais_b, cadeia)
+  return automato_1(funcao_transicao_b, estado_inicial_b, estados_finais_b, cadeia)
 
 
 # LETRA C 
@@ -69,25 +97,34 @@ def automato_b(cadeia:str):
 # LETRA D 
 
 def automato_d (cadeia:str):
+  '''
+   Validação de cadeia no automato da primeira questão, letra D
+   É usado uma chamada de outra função (automato), para a criação do automato,
+   sendo passado o automato desenvolvido para a alternativa
+   
+   Args: 
+    cadeia de string a ser analizada no automato criado
+
+   Return: 
+    Return da função automato. Print da transições utilizadas.
+    Valor booleano da validação da cadeia.
+  '''
   estados_d= ['q0','q1','q2','q3']
   alfabeto_d= ['a','b','c']
   funcao_transicao_d= {
       ('q0','a'):'q1',
       ('q0','b'):'q2',
-      ('q0','c'):None,
       ('q1','a'):'q1',
       ('q1','b'):'q2',
       ('q1','c'):'q3',
       ('q2','a'):'q3',
       ('q2','b'):'q2',
-      ('q2','c'):None,
-      ('q3','a'):None,
-      ('q3','b'):None,
       ('q3','c'):'q3',
   }
   estado_inicial_d= 'q0'
   estados_finais_d=['q1','q3']
   
-  return automato(funcao_transicao_d,estado_inicial_d,estados_finais_d,cadeia)
+  return automato_1(funcao_transicao_d,estado_inicial_d,estados_finais_d,cadeia)
+
 
 
--- Projeto/Questao_2/segunda_questao.py
+++ Projeto/Questao_2/segunda_questao.py
@@ -1,13 +1,41 @@
-def automato(funcao_transicao,q0,conj_est_finais, cadeia):
+def automato_2(funcao_transicao,q0,conj_est_finais, cadeia):
+    '''
+      Gerador de automato 
+      Args: 
+        Transições presentes no automato -> um dicionario com strings. Tendo do lado direito o estado e o simbolo a ser analisado.
+        lado esquerdo, para qual estado irá. 
+
+        q0 -> estado inicial do automato
+
+        Estados finais do automato -> string
+
+        cadeia -> sequencia de string a ser analizada pelo automato criado
+
+      Return:
+
+        Valor booleano para a validação da cadeia para com o automato 
+    '''
     estado_atual=q0
     for simbolo in cadeia:
-        # print(f'({estado_atual}, {simbolo}) ---> {funcao_transicao.get((estado_atual, simbolo))}')
         estado_atual=funcao_transicao.get((estado_atual, simbolo))
         if estado_atual == None:
             break
     return estado_atual in conj_est_finais
 
-def automato_segunda_questao (cadeia:str): 
+def automato_segunda_questao(cadeia:str): 
+    '''
+    Validação de cadeia no automato da segunda questão.
+   É usado uma chamada de outra função (automato_2), para a criação do automato,
+   sendo passado o automato desenvolvido para a questão.
+   
+   Args: 
+    cadeia de string a ser analizada no automato criado
+
+   Return: 
+    Return da função automato. Valor booleano da validação da cadeia.
+    
+    '''
+
     estados= ['q0','q1','q2','q3','q4','q5','q6','q7','q8','q9','q10']
     alfabeto=['c','o','m','p','u','t','a','d','o','r']
     funcao_transicao={
@@ -25,9 +53,19 @@ def automato_segunda_questao (cadeia:str):
     estado_inicial='q0'
     estados_finais=['q10']
     
-    return automato(funcao_transicao,estado_inicial,estados_finais,cadeia)
+    return automato_2(funcao_transicao,estado_inicial,estados_finais,cadeia)
 
 def segunda_questao():
+    '''
+    Ver a ocorrência da palavra "computador", no texto ja definido
+    Args: 
+     
+     Não possui
+    
+    Return: 
+     Quantidade de vez em que a palavra aparece no texto, e suas respectivas posições
+    
+    '''
     texto = 'O computador é uma máquina capaz de variados tipos de tratamento automático de informações ou processamento de dados. Entende-se por computador um sistema físico que realiza algum tipo de computação. Assumiu-se que os computadores pessoais e laptops são ícones da era da informação. O primeiro computador eletromecânico foi construído por Konrad Zuse (1910–1995). Atualmente, um microcomputador é também chamado computador pessoal ou ainda computador doméstico.'
     posicoes=[]
     cont=0
